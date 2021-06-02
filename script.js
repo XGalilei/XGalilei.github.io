aa96b37502a1dd33c075d3a5b5c4bd1fe171d84d
XGalilei <47959336+XGalilei@users.noreply.github.com>
2021-06-02 09:52:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+var tabs = document.getElementsByClassName("tab");
+var i;
 
+for (i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    
+    var panel = this.nextElementSibling;
+    if(panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
